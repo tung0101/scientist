dd3b6af4b29e4979f0eb365f5b30f23177d183ba
Nguyễn Doãn Tùng <95358274+tung0101@users.noreply.github.com>
2023-10-21 12:38:38 +0700

    21/10/2023 12h30

---
+++ src/App.js
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <h1
+      className="
+      text-4xl
+      text-center
+      text-blue-400
+      font-bold
+      mt-3
+    "
+    >
+      asd
+    </h1>
+  );
+};
+
+export default App;
