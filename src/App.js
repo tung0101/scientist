52ad094fe894bac19dacfc632709d3a4148294a1
unknown <truongphi754@gmail.com>
2023-11-03 22:27:58 +0700

    feat: update new code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import Sidebar from "./components/sidebar";
-import Navbar from "./components/navbar";
 import List from "./components/list";
 const App = () => {
   return (
-   <div className="flex">
-        <div className="sidebar">
-              <Sidebar/>
-        </div>
-        <div className="flex-grow">
-              <div className="flex flex-col h-screen">
-                    <Navbar/>
-                    <div className="content flex-grow bg-black text-white">
-                     <List/>
-                    </div>
-              </div>
-        </div>
-   </div>
+      <List/>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      "dark": "#2F3338",
-      
-    },
-   
+       
   },
   plugins: [],
 }
