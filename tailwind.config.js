8640caaedc530b73242b6009e7fff84510ced07d
unknown <truongphi754@gmail.com>
2023-11-07 08:43:09 +0700

    update new file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-<<<<<<< HEAD
       colors: {
         primary: {
           // dark: "#2F3338",
@@ -11,10 +10,6 @@ module.exports = {
 
         },
       },
-=======
-      "dark": "#2F3338",
-      
->>>>>>> 1295295a5b00974e51e1d6eb57a970ac3852ee50
     },
   },
   plugins: [require("tailwind-scrollbar")],
